c99065f09cff582f25b33248090e381a4f44e313
RAFI ODY PRASETYO <rafiody16@gmail.com>
2025-02-21 08:43:46 +0700

    Mengubah Tampilan Web

--- project_laravel/resources/views/welcome.blade.php
+++ project_laravel/resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PWL 2024</title>
+        <title>PWL 2025</title>
     </head>
     <body>
-        <h2>Pemrograman Web Lanjut 2024</h2>
+        <h2>Pemrograman Web Lanjut 2025</h2>
         <h3>Semangat Belajar Laravel 10</h3>
     </body>
 </html>
