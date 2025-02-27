985a218e7257cd41910c8e462e805e0053a5bb7d
RAFI ODY PRASETYO <rafiody16@gmail.com>
2025-02-27 21:20:27 +0700

    Blade View

--- Jobsheet2/PWL_2025/routes/web.php
+++ Jobsheet2/PWL_2025/routes/web.php
@@ -78,7 +78,12 @@ Route::resource('photos', PhotoController::class);
 
 Route::resource('photos', PhotoController::class)->only([
     'index', 'show'
-   ]);
-   Route::resource('photos', PhotoController::class)->except([
+]);
+Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
-   ]);
+]);
+
+Route::get('/greeting', function () {
+    return view('hello', ['name' => 'Rafi']);
+});
+
