3d960a8eae07a38236c60f205fd94fd86740f9b6
RAFI ODY PRASETYO <rafiody16@gmail.com>
2025-02-27 22:41:54 +0700

    Make function

--- Jobsheet2/POS/app/Http/Controllers/SalesController.php
+++ Jobsheet2/POS/app/Http/Controllers/SalesController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class SalesController extends Controller
 {
-    //
+    public function index(){
+        return view('sales.index');
+    }
 }
--- Jobsheet2/POS/app/Http/Controllers/UserController.php
+++ Jobsheet2/POS/app/Http/Controllers/UserController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function profile($id, $name)
+    {
+        return view('user.profile')->with('id', $id)->with('name', $name);
+    }
 }
