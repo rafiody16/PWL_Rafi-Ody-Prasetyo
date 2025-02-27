36f27c6ceec33e2ae2a787d1cd6185c787f1bcf2
RAFI ODY PRASETYO <rafiody16@gmail.com>
2025-02-27 23:29:44 +0700

    Blade POS

---
+++ Jobsheet2/POS/resources/views/user/profile.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+    <h1>User Profile</h1>
+    <h3>ID: {{ $id }}</h3>
+    <h3>Name: {{ $name }}</h3>
+</body>
+</html>
--- Jobsheet2/POS/routes/web.php
+++ Jobsheet2/POS/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+Route::get('/sales', [SalesController::class, 'index']);
