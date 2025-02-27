c539dc6ca4edc3da26d500acfc2d03fd177f67f9
RAFI ODY PRASETYO <rafiody16@gmail.com>
2025-02-27 19:23:05 +0700

    Routing

--- Jobsheet2/PWL_2025/routes/web.php
+++ Jobsheet2/PWL_2025/routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return ('Selamat Datang');
+// });
+
+// Route::get('/hello', function() {
+//     return 'Hello World';
+// });
+
+Route::get('/world', function() {
+    return 'World';
 });
+
+// Route::get('/about', function() {
+//     return '2341720180 - Rafi Ody Prasetyo';
+// });
+
