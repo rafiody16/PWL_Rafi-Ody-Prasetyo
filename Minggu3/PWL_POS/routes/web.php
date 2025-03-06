7379ec9959112af97b556dc21604c7d0b1be236d
RAFI ODY PRASETYO <rafiody16@gmail.com>
2025-03-06 16:31:27 +0700

    Implementasi Query Builder

--- Minggu3/PWL_POS/routes/web.php
+++ Minggu3/PWL_POS/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Praktikum 4
 Route::get('/level', [LevelController::class, 'index']);
+
+// Praktikum 5
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+
