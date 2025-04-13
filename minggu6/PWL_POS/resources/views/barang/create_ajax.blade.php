fd3ae303d309e8ea7441621d2724ffbfaa5de4a8
rafiody16 <rafiody16@gmail.com>
2025-04-13 13:26:47 +0700

    Tugas - m_barang

---
+++ minggu6/PWL_POS/resources/views/barang/create_ajax.blade.php
@@ -0,0 +1,129 @@
+<form action="{{ url('barang/ajax') }}" method="POST" id="form-tambah">
+    @csrf
+    <div id="modal-master" class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data barang</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label>kategori Barang</label>
+                    <select name="kategori_id" id="kategori_id" class="form-control" required>
+                        <option value="">- Pilih kategori -</option>
+                        @foreach ($kategori as $l)
+                            <option value="{{ $l->kategori_id }}">{{ $l->kategori_nama }}</option>
+                        @endforeach
+                    </select>
+                    <small id="error-kategori_id" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
+                    <label>Kode Barang</label>
+                    <input value="" type="text" name="barang_kode" id="barang_kode" class="form-control" required>
+                    <small id="error-barang_kode" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
+                    <label>Nama Barang</label>
+                    <input value="" type="text" name="barang_nama" id="barang_nama" class="form-control" required>
+                    <small id="error-barang_nama" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
+                    <label class="col-1 control-label col-form-label">Harga Beli</label>
+                    <div class="col-11">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">Rp.</span>
+                            </div>
+                            <input type="number" class="form-control" id="harga_beli" name="harga_beli" value="" required>
+                            <small id="error-harga_beli" class="error-text form-text text-danger"></small>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-1 control-label col-form-label">Harga Jual</label>
+                    <div class="col-11">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">Rp.</span>
+                            </div>
+                            <input type="number" class="form-control" id="harga_jual" name="harga_jual" value="" required>
+                            <small id="error-harga_jual" class="error-text form-text text-danger"></small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+        </div>
+    </div>
+</form>
+<script>
+    $(document).ready(function() {
+        $("#form-tambah").validate({
+            rules: {
+                kategori_id: {
+                    required: true,
+                    number: true
+                },
+                barangname: {
+                    required: true,
+                    minlength: 3,
+                    maxlength: 20
+                },
+                nama: {
+                    required: true,
+                    minlength: 3,
+                    maxlength: 100
+                },
+                password: {
+                    required: true, 
+                    minlength: 6,
+                    maxlength: 20
+                }
+            },
+            submitHandler: function(form) {
+                $.ajax({
+                    url: form.action,
+                    type: form.method,
+                    data: $(form).serialize(),
+                    success: function(response) {
+                        if (response.status) {
+                            $('#myModal').modal('hide');
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Berhasil',
+                                text: response.message
+                            });
+                            dataBarang.ajax.reload();
+                        } else {
+                            $('.error-text').text('');
+                            $.each(response.msgField, function(prefix, val) {
+                                $('#error-' + prefix).text(val[0]);
+                            });
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Terjadi Kesalahan',
+                                text: response.message
+                            });
+                        }
+                    }
+                });
+                return false;
+            },
+            errorElement: 'span',
+            errorPlacement: function(error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            }
+        });
+    });
+</script>
