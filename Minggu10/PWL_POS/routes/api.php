cccff879ce03fceee928c8d8d1722729bdec1f32
RAFI ODY PRASETYO <53549077+rafiody16@users.noreply.github.com>
2025-05-01 10:40:27 +0700

    Praktikum 4 - Implementasi CRUD dalam RESTful API

--- Minggu10/PWL_POS/routes/api.php
+++ Minggu10/PWL_POS/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,9 @@ Route::post('/logout', LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
